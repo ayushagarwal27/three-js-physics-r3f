13607f4859a98cb65ace3adfaeebae90dfbfb1dd
ayushagarwal27 <ayushagarwal.dev@gmail.com>
2023-08-29 08:54:34 +0530

    till colliders

---
+++ src/Experience.jsx
@@ -0,0 +1,124 @@
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  Physics,
+  RigidBody,
+  CuboidCollider,
+  CylinderCollider,
+} from "@react-three/rapier";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+
+export default function Experience() {
+  const [hitSound, _] = useState(() => {
+    return new Audio("./hit.mp3");
+  });
+
+  const cubeRef = useRef();
+  const twisterRef = useRef();
+
+  const model = useGLTF("./hamburger.glb");
+
+  const cubeJump = () => {
+    const mass = cubeRef.current.mass();
+    cubeRef.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 });
+    cubeRef.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const eulerRotation = new THREE.Euler(0, time * 3, 0);
+    const quartronionRotation = new THREE.Quaternion();
+    quartronionRotation.setFromEuler(eulerRotation);
+    twisterRef.current.setNextKinematicRotation(quartronionRotation);
+
+    const angle = time * 0.5;
+    const x = Math.cos(angle);
+    const z = Math.sin(angle);
+    twisterRef.current.setNextKinematicTranslation({ x, y: -0.8, z });
+  });
+
+  const collisonEnter = () => {
+    // hitSound.currentTime = 0;
+    // hitSound.volume = Math.random();
+    // hitSound.play();
+  };
+
+  return (
+    <>
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+
+      <ambientLight intensity={0.5} />
+
+      <Physics debug={true} gravity={[0, -9.08, 0]}>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[-1.5, 2, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody
+          position={[1.5, 2, 0]}
+          ref={cubeRef}
+          gravityScale={1}
+          restitution={0}
+          friction={0.7}
+          colliders={false}
+          onCollisionEnter={collisonEnter}
+          // onCollisionExit={() => {
+          //   console.log("exit");
+          // }}
+        >
+          <mesh castShadow onClick={cubeJump}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+          <CuboidCollider args={[0.5, 0.5, 0.5]} mass={2} />
+        </RigidBody>
+
+        {/* <RigidBody
+          colliders={false}
+          rotation={[Math.PI * 0.5,0,0]}
+          position={[0, 1, 0]}
+        >
+          <CuboidCollider args={[1.5, 1.5, 0.5]} />
+          <mesh castShadow>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="lightpurple" />
+          </mesh>
+        </RigidBody> */}
+
+        <RigidBody type="fixed" friction={0.7}>
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody position={[0, 4, 0]} colliders={false}>
+          <primitive object={model.scene} scale={0.25} />
+          <CylinderCollider args={[0.5, 1.25]} />
+        </RigidBody>
+        <RigidBody
+          position={[0, -0.8, 0]}
+          friction={0}
+          type="kinematicPosition"
+          ref={twisterRef}
+        >
+          <mesh castShadow scale={[0.4, 0.4, 3]}>
+            <boxGeometry />
+            <meshStandardMaterial color="red" />
+          </mesh>
+        </RigidBody>
+      </Physics>
+    </>
+  );
+}
